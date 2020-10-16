e684e0b519cf29169fae5df91d3ff2fbb86c8d21
quancheng <flandrexremilia@foxmail.com>
2020-10-16 22:16:39 +0800

    Implementation of Vector

---
+++ Vector/Vector.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <algorithm>
+
+template <typename T>
+class Vector {
+public:
+	explicit Vector(int initSize) : theSize{ initSize },
+		theCapacity{ initSize + SPARE_CAPACITY } {
+		objects = new T[theCapacity];
+	}
+
+	Vector(const Vector& rhs) : theSize{ rhs.theSize },
+		theCapacity{ rhs.theCapacity }, objects{ nullptr } {
+		objects = new T[theCapacity];
+		for (int k = 0; k < theSize; ++k)
+			objects[k] = rhs.objects[k];
+	}
+
+	Vector& operator=(const Vector& rhs) {
+		Vector copy = rhs;
+		std::swap(*this, copy);
+		return *this;
+	}
+
+	~Vector() {
+		delete[] objects;
+	}
+
+	Vector(Vector&& rhs) : theSize{ rhs.theSize },
+		theCapacity(rhs.theCapacity), objects{ rhs.objects } {
+		rhs.objects = nullptr;
+		rhs.theSize = 0;
+		rhs.theCapacity = 0;
+	}
+
+	Vector& operator=(Vector&& rhs) {
+		std::swap(theSize, rhs.theSize);
+		std::swap(theCapacity, rhs.theCapacity);
+		std::swap(objects, rhs.objects);
+		return *this;
+	}
+
+	void resize(int newSize) {
+		if (newSize > theCapacity)
+			reverse(newSize * 2);
+		theSize = newSize;
+	}
+
+	void reserve(int newCapacity) {
+		if (newCapacity < theSize)
+			return;
+
+		T* newArray = new T[newCapacity];
+		for (int k = 0; k < theSize; ++k)
+			T[k] = std::move(objects[k]);
+
+		theCapacity = newCapacity;
+		std::swap(objects, newArray);
+		delete[] newArray;
+	}
+
+	T& operator[](int index) {
+		return objects[index];
+	}
+
+	static const int SPARE_CAPACITY = 16;
+
+private:
+	int theSize;
+	int theCapacity;
+	T* objects;
+};
