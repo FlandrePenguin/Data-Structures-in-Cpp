51f7522110f9d552cd8cf19c17c896fba7392200
quancheng <flandrexremilia@foxmail.com>
2020-10-16 23:45:49 +0800

    Implementation of Vector

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -63,6 +63,61 @@ public:
 		return objects[index];
 	}
 
+	const T& operator[](int index) const {
+		return objects[index];
+	}
+
+	bool empty() const {
+		return size() == 0;
+	}
+
+	int size() const {
+		return theSize;
+	}
+
+	int capacity() const {
+		return theCapacity;
+	}
+
+	void push_back(const T& x) {
+		if (theSize == theCapacity)
+			reserve(2 * theCapacity + 1);
+		objects[theSize++] = x;
+	}
+
+	void push_back(T&& x) {
+		if (theSize == theCapacity)
+			reserve(2 * theCapacity + 1);
+		objects[theSize++] = std::move(x);
+	}
+
+	void pop_back() {
+		--theSize;
+	}
+
+	const T& back() const {
+		return objects[theSize - 1];
+	}
+
+	using iterator = T*;
+	using const_iterator = const T*;
+
+	iterator begin() {
+		return &objects[0];
+	}
+
+	const_iterator begin() const {
+		return &objects[0];
+	}
+
+	iterator end() {
+		return &objects[size()];
+	}
+
+	const_iterator end() const {
+		return &objects[size()];
+	}
+
 	static const int SPARE_CAPACITY = 16;
 
 private:
